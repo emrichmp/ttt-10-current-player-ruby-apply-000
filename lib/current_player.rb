eadc04b982b5006154a11ce853985390590aaa25
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-05-13 02:10:13 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,22 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def turn_count(board)
+  count = 0
+  board.each do |slot|
+    if slot == " " || if slot == ""
+      count += 1
+    end
+  end
+  end
+  puts "#{count}"
+  return count
+end
+
+turn_count(board)
+
+def current_player(count)
+  if count % 2
+    return "O"
+  else
+    return "X"
+  end
+end
